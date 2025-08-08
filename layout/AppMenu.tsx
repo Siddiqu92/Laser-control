f19347638a0c30a5d23b46941464f012b3762d69
Muhammad Siddique <siddiqkhaan92@gmail.com>
2025-08-08 16:13:33 +0500

    Initial commit

---
+++ layout/AppMenu.tsx
@@ -0,0 +1,57 @@
+import type { MenuModel } from "@/types";
+import AppSubMenu from "./AppSubMenu";
+
+const AppMenu = () => {
+    const model: MenuModel[] = [
+        {
+            label: "Dashboards",
+            icon: "pi pi-home",
+            items: [
+                {
+                    label: "Schools",
+                    icon: "pi pi-fw pi-building",
+                    to: "/schools",
+                },
+                {
+                    label: "Teachers",
+                    icon: "pi pi-fw pi-users",
+                    to: "/teachers",
+                },
+                {
+                    label: "Students",
+                    icon: "pi pi-fw pi-user",
+                    to: "/students",
+                },
+                {
+                    label: "Programs of Study",
+                    icon: "pi pi-fw pi-book",
+                    to: "/programs-of-study",
+                },
+                {
+                    label: "Courses",
+                    icon: "pi pi-fw pi-briefcase",
+                    to: "/courses",
+                },
+                {
+                    label: "Devices",
+                    icon: "pi pi-fw pi-tablet",
+                    to: "/devices",
+                },
+                {
+                    label: "Settings",
+                    icon: "pi pi-fw pi-cog",
+                    to: "/settings",
+                },
+                {
+                    label: "Login",
+                    icon: "pi pi-fw pi-sign-in",
+                    to: "/auth/login",
+                },
+            ],
+        },
+    ];
+
+    return <AppSubMenu model={model} />;
+};
+
+export default AppMenu;
